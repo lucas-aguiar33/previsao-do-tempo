68e3f653cf39bd0d621a77a790e02efd456e15c2
Lucas Aguiar <lucasaguiar.98@outlook.com>
2025-06-18 19:44:34 -0300

    reajustando as cores e adicionando temperaturas máx e min

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ const key = "750c0a6c427e5a8b6a6d2d7b3c8c7279";
 const btn_buscar = document.querySelector(".btn-busca");
 
 function exibirDados(dados){
-    document.querySelector('.cidade').innerHTML = "Tempo em " + dados.name;
+    console.log(dados);
+    document.querySelector('.cidade').innerHTML = dados.name;
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".min").innerHTML = Math.floor(dados.main.temp_min) + "°"
+    document.querySelector(".max").innerHTML = Math.floor(dados.main.temp_max) + "°"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
     document.querySelector(".umidade").innerHTML = "Umidade: "+dados.main.humidity + "%";
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png` 
@@ -18,6 +21,8 @@ async function buscarCidade(cidade) {
 }
 
 btn_buscar.addEventListener("click",()=>{
-    const cidade = document.querySelector(".input-cidade").value;
-    buscarCidade(cidade);
+    const cidade = document.querySelector(".input-cidade");
+    buscarCidade(cidade.value);
+    
+    cidade.value = "";
 })
